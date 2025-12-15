41a89a8ba05b080dd931bfa2eb046f04095f5465
Robert Danilo <109989489+RD0705@users.noreply.github.com>
2025-12-15 16:20:48 +0000

    Atualização de Botões e UI

---
+++ .next/static/chunks/app_sobre_page_tsx_d6bb30._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_sobre_page_tsx_d6bb30._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_sobre_page_tsx_d6bb30._.js",
+  "chunks": [
+    "static/chunks/_56ab21._.js"
+  ],
+  "source": "dynamic"
+});
